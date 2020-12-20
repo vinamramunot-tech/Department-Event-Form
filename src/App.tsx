160633ce3562ff6859ef63a2939eb293496404fe
Mason Adkins <adkimb01@pfw.edu>
2020-12-20 14:05:22 -0500

    feat: initial form

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,103 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import icon from '../assets/icon.svg';
+import { useForm } from "react-hook-form";
 
-const Hello = () => {
+function Form() {
+  const {register, handleSubmit} = useForm(); 
+
+  const onSubmit = data => {
+    console.log(data)
+
+    let filename = "export.json";
+    let contentType = "application/json;charset=utf-8;";
+    if (window.navigator && window.navigator.msSaveOrOpenBlob) {
+      var blob = new Blob([decodeURIComponent(encodeURI(JSON.stringify(data)))], { type: contentType });
+      navigator.msSaveOrOpenBlob(blob, filename);
+    } else {
+      var a = document.createElement('a');
+      a.download = filename;
+      a.href = 'data:' + contentType + ',' + encodeURIComponent(JSON.stringify(data, null, 1));
+      a.target = '_blank';
+      a.click();
+    }
+    
+  }
+
   return (
-    <div>
-      <div className="Hello">
-        <img width="200px" alt="icon" src={icon} />
-      </div>
-      <h1>electron-react-boilerplate</h1>
-      <div className="Hello">
-        <a
-          href="https://electron-react-boilerplate.js.org/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="books">
-              📚
-            </span>
-            Read our docs
-          </button>
-        </a>
-        <a
-          href="https://github.com/sponsors/electron-react-boilerplate"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="books">
-              🙏
-            </span>
-            Donate
-          </button>
-        </a>
-      </div>
-    </div>
+    <form onSubmit = {handleSubmit(onSubmit)}>
+      <label htmlFor = "food_plan">Food Plans </label>
+      <input name = 'food_plan' id = "food_plan" type = 'Text' ref = {register}/> <br></br>
+
+      <label htmlFor = "who_pays">Who Pays </label>
+      <input name = "who_pays" id = "who_pays" type = 'Text' ref = {register}/><br></br>
+
+      {/* <input type="Text" id="acm" name="acm" ref = {register}/> 
+      <label htmlFor="acm"> ACM</label><br></br>
+      <input type="Text" id="dept" name="dept" ref = {register}/>
+      <label htmlFor="dept"> DEPT</label><br></br>
+      <input type="Text" id="students" name="students" ref = {register}/> 
+      <label htmlFor="students"> STUDENTS</label><br></br> */}
+      
+      <label htmlFor = "event_costs">Event Costs </label>
+      <input name = "event_costs"  id = "event_costs" type = 'Number' ref = {register}/><br></br>
+
+      <label htmlFor = "many_students">How Many Students </label>
+      <input name = "many_students" id = "many_students" type = 'Number' ref = {register}/><br></br>
+
+      <label htmlFor = "team_decision">What Decides The Teams </label>
+      <input name = "team_decision" id = "team_decision" type = 'Text' ref = {register}/><br></br>
+
+      <label htmlFor = "many_teams">How Many Teams </label>
+      <input name = "many_teams" id = "many_teams" type = 'Number' ref = {register}/><br></br>
+
+      <label htmlFor = "entry_rules">What Are The Rules For Entry </label>
+      <input name = "entry_rules" id = "entry_rules" type = 'Text' ref = {register}/><br></br>
+
+      <label htmlFor = "timeline">Timelines </label>
+      <input name = "timeline" id = "timeline" type = 'Text' ref = {register}/> <br></br>
+
+      <label htmlFor = "when_handled">When Should Things Be Handled </label>
+      <input name = "when_handled" id = "when_handled" type = 'Text' ref = {register}/><br></br>
+
+      <label htmlFor = "deadline">Deadlines </label>
+      <input name = "deadline" id = "deadline" type = 'Text' ref = {register}/><br></br>
+
+      <label htmlFor = "fundraise">Fundraising </label>
+      <input name = "fundraise" id = "fundraise" type = 'Text' ref = {register}/><br></br>
+
+      <label htmlFor = "advert">Advertising </label>
+      <input name = "advert" id = "advert" type = 'Text' ref = {register}/> <br></br>
+
+      <label htmlFor = "how_advert">How To Advertise </label>
+      <input name = "how_advert" id = "how_advert" type = 'Text' ref = {register}/><br></br>
+
+      <label htmlFor = "pro_photos">Getting Professional Photos </label>
+      <input name = "pro_photos" id = "pro_photos" type = 'Text' ref = {register}/><br></br>
+
+      <label htmlFor = "coach">Coaching </label>
+      <input name = "coach" id = "coach" type = 'Text' ref = {register}/> <br></br>
+
+      <label htmlFor = "who_coach">Who Coaches </label>
+      <input name = "who_coach" id = "who_coach" type = 'Text' ref = {register}/><br></br>
+
+      <label htmlFor = "coach_num">How Many Coaches </label>
+      <input name = "coach_num" id = "coach_num" type = 'Number' ref = {register}/><br></br>
+
+      <label htmlFor = "pre_comp">Pre-competition </label>
+      <input name = "pre_comp" id = "pre_comp" type = 'Text' ref = {register}/> <br></br>
+
+      <label htmlFor = "workshop">Workshops </label>
+      <input name = "workshop" id = "workshop" type = 'Text' ref = {register}/><br></br>
+
+      <label htmlFor = "training">Training </label>
+      <input name = "training" id = "training" type = 'Text' ref = {register}/><br></br>
+
+      <label htmlFor = "club_lectures">Club Sponsored Lectures </label>
+      <input name = "club_lectures" id = "club_lectures" type = 'Text' ref = {register}/><br></br>
+
+      <input type = 'submit' /> submit form
+
+    </form>
   );
 };
 
@@ -43,7 +105,7 @@ export default function App() {
   return (
     <Router>
       <Switch>
-        <Route path="/" component={Hello} />
+        <Route path="/" component={Form} />
       </Switch>
     </Router>
   );
