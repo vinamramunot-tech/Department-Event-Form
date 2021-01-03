8eafe9000b10cd1bbf588d01e43595186818eb8f
vinamramunot-tech <munov01@pfw.edu>
2021-01-02 19:22:04 -0500

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,108 +1,60 @@
-import React, { FormEvent, useState } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { createStyles, makeStyles, TextField, Theme } from '@material-ui/core';
-import { purple } from '@material-ui/core/colors';
+import { Form, Input, Button, Checkbox } from 'antd';
 
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    root: {
-      '& .MuiTextField-root': {
-        margin: theme.spacing(1),
-        width: '25ch',
-      },
-      color: 'white',
-    },
-  })
-);
+function FormDiv() {
 
-function Form() {
-  const [whoPaysForFood, setWhoPaysForFood] = useState('');
-  const [whereIsFoodComingFrom, setWhereIsFoodComingFrom] = useState('');
-  const [numberOfStudents, setNumberOfStudents] = useState(0);
-  const [numberOfTeams, setNumberOfTeams] = useState(0);
-  const [rulesForEntry, setRulesForEntry] = useState('');
-  const [deadline, setDeadline] = useState('');
-  const [advertisement, setAdvertisementPlan] = useState('');
-  const [coach, setCoach] = useState('');
-  const [numberOfCoaches, setNumberOfCoaches] = useState('');
-  const [workshops, setWorkshops] = useState('');
-  const [training, setTraining] = useState('');
-  const [clubSponsoredLectures, setClubSponsoredLectures] = useState('');
-
-  const classes = useStyles();
-
-  const downloadReport = (event: FormEvent) => {
-    event.preventDefault();
-    
-    // const data = null;
-    // const filename = 'export.json';
-    // const contentType = 'application/json;charset=utf-8;';
-    // if (window.navigator && window.navigator.msSaveOrOpenBlob) {
-    //   const blob = new Blob(
-    //     [decodeURIComponent(encodeURI(JSON.stringify(data)))],
-    //     { type: contentType }
-    //   );
-    //   navigator.msSaveOrOpenBlob(blob, filename);
-    // } else {
-    //   const a = document.createElement('a');
-    //   a.download = filename;
-    //   a.href = `data:${contentType},${encodeURIComponent(
-    //     JSON.stringify(data, null, 1)
-    //   )}`;
-    //   a.target = '_blank';
-    //   a.click();
-    // }
+  const layout = {
+    labelCol: { span: 8 },
+    wrapperCol: { span: 16 },
   };
+  const tailLayout = {
+    wrapperCol: { offset: 8, span: 16 },
+  };
+  
+  const Demo = () => {
+    const onFinish = (values: any) => {
+      console.log('Success:', values);
+    };
+  
+    const onFinishFailed = (errorInfo: any) => {
+      console.log('Failed:', errorInfo);
+    };
 
   return (
-    <form className={classes.root} onSubmit={downloadReport}>
-      <div>
-        <TextField
-          id="whereIsFoodComingFrom"
-          label="Where is Food Coming From?"
-          onChange={(e) => setWhereIsFoodComingFrom(e.target.value)}
-        />
-        <TextField
-          id="whoPaying"
-          label="Who Pays"
-          onChange={(e) => setWhoPaysForFood(e.target.value)}
-        />
-      </div>
-      <div>
-        <TextField id="eventCosts" label="Event costs" type="Number" />
-        <TextField id="numOfStudents" label="How many students" type="Number" />
-        <TextField id="teamDecision" label="What decides the teams" />
-        <TextField id="numOfTeams" label="How many teams" type="Number" />
-        <TextField id="entryRules" label="What are the rules for entry" />
-      </div>
-      <div>
-        <TextField id="timeline" label="Timelines" />
-        <TextField id="whenHandled" label="When Should Things Be Handled" />
-        <TextField id="deadline" label="Deadline" />
-        <TextField id="fundraise" label="Fundraising" />
-      </div>
-      <div>
-        <TextField id="advertising" label="Advertising" />
-        <TextField id="howToAdvert" label="How to Advertise" />
-        <TextField id="proPhotos" label="Getting professional photos" />
-      </div>
-      <div>
-        <TextField id="coach" label="Coaching" />
-        <TextField id="whoCoaches" label="Who Coaches" />
-        <TextField
-          id="numberOfCoaches"
-          label="How many Coaches"
-          type="Number"
-        />
-      </div>
-      <div>
-        <TextField id="preComp" label="Pre Competition" />
-        <TextField id="workshop" label="Workshops" />
-        <TextField id="training" label="Training" />
-        <TextField id="clubLectures" label="Club Sponsored Lectures" />
-      </div>
-      <input type="submit" value="Download" id="bttn" />
-    </form>
+    <Form
+      {...layout}
+      name="basic"
+      initialValues={{ remember: true }}
+      onFinish={onFinish}
+      onFinishFailed={onFinishFailed}
+    >
+      <Form.Item
+        label="Username"
+        name="username"
+        rules={[{ required: true, message: 'Please input your username!' }]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item
+        label="Password"
+        name="password"
+        rules={[{ required: true, message: 'Please input your password!' }]}
+      >
+        <Input.Password />
+      </Form.Item>
+
+      <Form.Item {...tailLayout} name="remember" valuePropName="checked">
+        <Checkbox>Remember me</Checkbox>
+      </Form.Item>
+
+      <Form.Item {...tailLayout}>
+        <Button type="primary" htmlType="submit">
+          Submit
+        </Button>
+      </Form.Item>
+    </Form>
   );
 }
 
@@ -110,7 +62,7 @@ export default function App() {
   return (
     <Router>
       <Switch>
-        <Route path="/" component={Form} />
+        <Route path="/" component={FormDiv} />
       </Switch>
     </Router>
   );
