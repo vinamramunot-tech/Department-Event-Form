6882c19034edbdf983973bed570bdee6cb1d3d1d
vinamramunot-tech <munov01@pfw.edu>
2021-01-06 20:29:43 -0500

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,22 @@
+/* eslint-disable react/jsx-no-undef */
 /* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { Form, Input, Button, Checkbox } from 'antd';
+import { Form, Input, Button, Select } from 'antd';
 
 const layout = {
   labelCol: {
-    span: 8,
+    offset: 4,
+    span: 9,
   },
   wrapperCol: {
-    span: 16,
+    span: 8,
   },
 };
 const tailLayout = {
   wrapperCol: {
-    offset: 2,
-    span: 16,
+    offset: 12,
+    span: 8,
   },
 };
 
@@ -38,16 +40,37 @@ const FormDiv = () => {
       onFinishFailed={onFinishFailed}
     >
       <Form.Item
-        label="Username"
-        name="username"
+        label="Who Covers the first Meal"
+        name="firstMeal"
         rules={[
           {
             required: true,
-            message: 'Please input your username!',
+            message: 'Please enter who is going to cover for first meal?',
           },
         ]}
       >
-        <Input />
+        <Select placeholder="Select an option">
+          <Option value="noMeal">No Meal</Option>
+          <Option value="department">Department</Option>
+          <Option value="ACM">ACM</Option>
+          <Option value="self">Self</Option>
+        </Select>
+      </Form.Item>
+
+      <Form.Item
+        label="Who Covers the second Meal"
+        name="secondMeal"
+        rules={[
+          {
+            message: 'Please enter who is going to cover for the second meal?',
+          },
+        ]}
+      >
+        <Select placeholder="Select an option">
+          <Option value="No meal">No Meal</Option>
+          <Option value="ACM">ACM</Option>
+          <Option value="self">Self</Option>
+        </Select>
       </Form.Item>
       <br />
       <Form.Item
@@ -65,7 +88,7 @@ const FormDiv = () => {
 
       <Form.Item {...tailLayout}>
         <Button type="primary" htmlType="submit">
-          Submit
+          Download
         </Button>
       </Form.Item>
     </Form>
